1920d2391a102f6a7b7acc5ded9c284e45e06f5f
Narek <narek.alexanyan.96@gmail.com>
2025-02-10 19:09:17 +0400

    Added tailwind classes, lucid icons, navbar section

---
+++ src/constants/index.ts
@@ -0,0 +1,24 @@
+import {NavLink} from "../types";
+
+export const navLinks: NavLink[] = [
+    {
+        id: 1,
+        name: 'Home',
+        href: '#home',
+    },
+    {
+        id: 2,
+        name: 'About',
+        href: '#about',
+    },
+    {
+        id: 3,
+        name: 'Work',
+        href: '#work',
+    },
+    {
+        id: 4,
+        name: 'Contact',
+        href: '#contact',
+    },
+];
