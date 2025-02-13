00e594033664fd3a0d0ab884576d7ade8bb0fd91
Narek <narek.alexanyan.96@gmail.com>
2025-02-13 18:29:34 +0400

    Created experience section, added developer 3d avatar

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,6 @@
+import * as THREE from 'three'
 import {Material, Mesh} from "three";
+import { GLTF } from "three/examples/jsm/Addons.js";
 
 export interface NavLink {
     id: number,
@@ -77,4 +79,41 @@ export interface clientReview {
     position: string,
     img: string,
     review: string,
+}
+
+export interface workExperience {
+    id: number,
+    name: string,
+    pos: string,
+    duration: string,
+    title: string,
+    icon: string,
+    animation: string,
+}
+
+export type GLTFResult = GLTF & {
+    nodes: {
+        EyeLeft: THREE.SkinnedMesh
+        EyeRight: THREE.SkinnedMesh
+        Wolf3D_Head: THREE.SkinnedMesh
+        Wolf3D_Teeth: THREE.SkinnedMesh
+        Wolf3D_Hair: THREE.SkinnedMesh
+        Wolf3D_Glasses: THREE.SkinnedMesh
+        Wolf3D_Body: THREE.SkinnedMesh
+        Wolf3D_Outfit_Bottom: THREE.SkinnedMesh
+        Wolf3D_Outfit_Footwear: THREE.SkinnedMesh
+        Wolf3D_Outfit_Top: THREE.SkinnedMesh
+        Hips: THREE.Bone
+    }
+    materials: {
+        Wolf3D_Eye: THREE.MeshStandardMaterial
+        Wolf3D_Skin: THREE.MeshStandardMaterial
+        Wolf3D_Teeth: THREE.MeshStandardMaterial
+        Wolf3D_Hair: THREE.MeshStandardMaterial
+        Wolf3D_Glasses: THREE.MeshStandardMaterial
+        Wolf3D_Body: THREE.MeshStandardMaterial
+        Wolf3D_Outfit_Bottom: THREE.MeshStandardMaterial
+        Wolf3D_Outfit_Footwear: THREE.MeshStandardMaterial
+        Wolf3D_Outfit_Top: THREE.MeshStandardMaterial
+    }
 }
